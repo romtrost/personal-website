7ad1082f2e0999eb80b2ebed8345841329110ffa
Romain Trost <romainltrost@hotmail.com>
2023-09-06 20:00:46 +0200

    added animations for hero page + added new font

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {motion} from 'framer-motion'
 import '../App.css';
 import './HeroSection.css';
 import Navbar from './Navbar';
@@ -9,11 +10,34 @@ function HeroSection() {
       <img src='/images/header-background.jpg'  />
       <Navbar />
       <div className='hero-container'>
-        <h1>Hello! I'm</h1>
-        <h2>Romain Trost</h2>
-        <p>A Data Scientist and Tech Enthusiast from Belgium</p>
+        <motion.h1  initial={{ y: 70, opacity: 0, scale: 1 }}
+                    animate={{ y: 0, opacity: 1, scale: 1 }}
+                    transition={{
+                      duration: 0.5,
+                      delay: 0.3,
+                      ease: [0.1, 0.4, 0.2, 1.0]
+                    }}>
+                    HELLO! I'M
+        </motion.h1>
+        <motion.h2  initial={{ y: 70, opacity: 0, scale: 1 }}
+                    animate={{ y: 0, opacity: 1, scale: 1 }}
+                    transition={{
+                      duration: 0.5,
+                      delay: 0.35,
+                      ease: [0.1, 0.4, 0.2, 1.0]
+                    }}>
+          ROMAIN TROST
+        </motion.h2>
+        <motion.p initial={{ opacity: 0, scale: 1 }}
+                  animate={{ opacity: 1, scale: 1 }}
+                  transition={{
+                    duration: 0.5,
+                    delay: 1.8,
+                    ease: "easeOut"
+                  }}>
+          A Data Scientist and Tech Enthusiast from Belgium
+        </motion.p>
       </div>
-      
     </div>
 
   );
