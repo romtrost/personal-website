567f1efedc4787583e1e012e9cc2f291c90589a5
Romain Trost <romainltrost@hotmail.com>
2023-09-08 17:04:27 +0200

    addes parallel scrolling effect

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,11 +1,91 @@
-import React from 'react';
-import {motion} from 'framer-motion'
+import React, { useRef }  from 'react';
+import { motion, useScroll, useTransform } from "framer-motion";
 import '../App.css';
 import './HeroSection.css';
 import Navbar from './Navbar';
 
 function HeroSection() {
+
+  const ref = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start start", "end start"],
+  });
+  const backgroundY = useTransform(scrollYProgress, [0, 1], ["0%", "10%"]);
+  const textY = useTransform(scrollYProgress, [0, 1], ["0%", "500%"]);
+
+  console.log(backgroundY)
+  console.log(textY)
+
   return (
+    
+    <div 
+    ref={ref}
+    className='hero'>
+      <Navbar />
+      <div className='hero-container'>
+        <motion.div className='hero-text-container'
+                    style={{ 
+                      zIndex: 10,
+                      y: textY 
+                    }}>
+          <motion.h1  initial={{ y: 500, opacity: 0, scale: 1 }}
+                      animate={{ y: 0, opacity: 1, scale: 1 }}
+                      transition={{
+                        duration: 1,
+                        delay: 0.3,
+                        ease: [0.1, 0.4, 0.2, 1.0]
+                      }}>
+                      HELLO! I'M
+          </motion.h1>
+          <motion.h2  initial={{ y: 500, opacity: 0, scale: 1 }}
+                      animate={{ y: 0, opacity: 1, scale: 1 }}
+                      transition={{
+                        duration: 1,
+                        delay: 0.35,
+                        ease: [0.1, 0.4, 0.2, 1.0]
+                      }}>
+                      ROMAIN TROST
+          </motion.h2>
+          <motion.p initial={{ opacity: 0, scale: 1 }}
+                    animate={{ opacity: 1, scale: 1 }}
+                    transition={{
+                      duration: 0.5,
+                      delay: 1.8,
+                      ease: "easeOut"
+                    }}>
+                    A Data Scientist and Tech Enthusiast from Belgium
+          </motion.p>
+        </motion.div>
+
+      </div>
+      <motion.img
+          src='/images/header-background.jpg'
+          style={{
+            position: 'absolute',
+            top: 0,
+            right: 0,
+            bottom: 0,
+            left: 0,
+            zIndex: 0,
+            y: backgroundY,
+          }}
+        />
+        <motion.img
+          src='/images/header-background-foreground.png'
+          style={{
+            position: 'absolute',
+            top: 0,
+            right: 0,
+            bottom: 0,
+            left: 0,
+            zIndex: 20,
+          }}
+        />
+      
+    </div>
+    
+    /*
     <div className='hero'>
       <img src='/images/header-background.jpg'  />
       <Navbar />
@@ -39,7 +119,7 @@ function HeroSection() {
         </motion.p>
       </div>
     </div>
-
+    */
   );
 }
 
