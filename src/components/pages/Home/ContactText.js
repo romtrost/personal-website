a3929ee00b7d6c49cd0bc7d5d0829fa4f03a802e
Romain Trost <romainltrost@hotmail.com>
2023-11-01 17:09:08 +0100

    fixed z index issue

--- src/components/pages/Home/ContactText.js
+++ src/components/pages/Home/ContactText.js
@@ -15,29 +15,27 @@ function ContactText(props) {
 
   return (
 
-    
-    <motion.p ref={ref}
-              variants={{
-                hidden: { y: 25, opacity: 0, scale: 1 },
-                visible: { y: 0, opacity: 1, scale: 1 },
-              }}
-              initial="hidden"
-              animate={mainControls}
-              transition={{
-                duration: 0.5,
-                delay: 0.3,
-                ease: [0.1, 0.4, 0.2, 1.0]
-              }}>
-              Reach out by e-mail if you want to connect! 
-              You can also find me on{' '}
-              <a className='contact-linkedin-text'
-                 href="https://www.linkedin.com/in/romain-trost-9a3955197/">Linkedin
-              </a> 
-              {' '}if you prefer.
-    </motion.p>
-    
-
-
+    <div style ={{display: "flex", justifyContent: "center"}}>
+      <motion.p ref={ref} style ={{display: "flex", alignItems: "center"}}
+                variants={{
+                  hidden: { y: 25, opacity: 0, scale: 1 },
+                  visible: { y: 0, opacity: 1, scale: 1 },
+                }}
+                initial="hidden"
+                animate={mainControls}
+                transition={{
+                  duration: 0.5,
+                  delay: 0.3,
+                  ease: [0.1, 0.4, 0.2, 1.0]
+                }}>
+                Reach out by e-mail if you want to connect! 
+                You can also find me on{' '}
+                  <a className='contact-linkedin-text'
+                    href="https://www.linkedin.com/in/romain-trost-9a3955197/">Linkedin
+                  </a> 
+                {' '}if you prefer.
+      </motion.p>         
+    </div>
 
   );
 }
